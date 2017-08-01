cae35d98a6eab15945bcb8ccad1736e52017906b
hagair <hagair@gettaxi.com>
2017-08-01 11:57:36 +0300

    change filename

--- webserver/src/main/java/com/webservice/controller/SettingsController.java
+++ webserver/src/main/java/com/webservice/controller/SettingsController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 
 @RestController
-public class WebController {
+public class SettingsController {
 
 	SystemSettings systemSettings;
 	Gson gson;
