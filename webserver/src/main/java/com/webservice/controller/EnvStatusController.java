047aedadbb21b128a19349595535f486922c3389
hagair <hagair@gettaxi.com>
2017-10-31 22:37:55 +0200

    1st commit

---
+++ webserver/src/main/java/com/webservice/controller/EnvStatusController.java
@@ -0,0 +1,30 @@
+package com.webservice.controller;
+
+import com.webservice.handler.SystemSettings;
+import com.utils.ResourcesHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+//import com.webservice.model.Customer;
+//import com.webservice.repo.CustomerRepository;
+import com.webservice.handler.EnvironmentCheck;
+
+@RestController
+public class EnvStatusController {
+    //	CustomerRepository repository;
+    EnvironmentCheck environmentCheck = new EnvironmentCheck();
+    SystemSettings systemSettings;
+
+    @RequestMapping("/statusJson")
+    public String serverStatusJ(@RequestParam("scrum") String scrum){
+        if (scrum.equals(null)) {
+            scrum = "scrum12";
+        }
+        String response = environmentCheck.getAllServersdetailsJson(scrum);
+        return response;
+    }
+
+}
+
---
+++ webserver/src/main/java/com/webservice/handler/EnvironmentCheck.java
@@ -0,0 +1,55 @@
+package com.webservice.handler;
+
+import com.google.gson.Gson;
+import com.settings.model.ServerDetails;
+import com.utils.ResourcesHandler;
+
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+
+import static com.utils.ResourcesHandler.getStringFromInputStream;
+
+/**
+ * Created by hagairevah on 6/1/17.
+ */
+public class EnvironmentCheck {
+    String charset = "UTF-8";
+    Gson gson = new Gson();
+
+    public ServerDetails getServerDetailsO (String scrum, String server) {
+        String url = "http://" + server + "-" + scrum + ".gett.io/alive";
+        try {
+            URLConnection connection = new URL(url + "").openConnection();
+            connection.setRequestProperty("Accept-Charset", charset);
+            InputStream response = connection.getInputStream();
+//            System.out.print("#");
+            System.out.print(server+"  ");
+            ServerDetails serverDetails = gson.fromJson(getStringFromInputStream(response), ServerDetails.class);
+            serverDetails.setServer(server);
+            return serverDetails;
+        } catch (Exception e) {
+            return new ServerDetails();
+        }
+    }
+
+    public String getAllServersdetailsJson(String scrum){
+        String s = "";
+        ArrayList<String> serverList = loadAllServers();
+        ArrayList<ServerDetails> serverListOb = new ArrayList();
+
+        for (int i = 0; i < serverList.size(); i++) {
+            String s1 =  serverList.get(i);
+            serverListOb.add(getServerDetailsO(scrum, s1));
+
+        }
+        return gson.toJson(serverListOb);
+    }
+    public ArrayList<String> loadAllServers()  {
+        String filename = "env.properties";
+        return ResourcesHandler.loadFromPropFile(filename);
+    }
+
+
+}
